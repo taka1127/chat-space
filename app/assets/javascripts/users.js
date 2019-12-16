646212a889c7c4b4aad1fdcb447373225bb0356a
taka1127 <taka07251127@gmail.com>
2019-12-17 00:30:12 +0900

    edit 2

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,17 @@
+$(function(){
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: { keyword: input }
+    })
+    .done(function(users) {
+      console.log("成功です");
+    })
+    .fail(function() {
+      console.log("失敗です");
+    });
+  });
+});
